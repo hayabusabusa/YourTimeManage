087ecd233a0498f2404ed883ab3f6379a10bba51
hayabusabusa <hayabusabusaSY@gmail.com>
2021-03-11 20:24:38 +0900

    chore: add model target and user defaults provider

---
+++ Model/Model.h
@@ -0,0 +1,18 @@
+//
+//  Model.h
+//  Model
+//
+//  Created by Shunya Yamada on 2021/03/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Model.
+FOUNDATION_EXPORT double ModelVersionNumber;
+
+//! Project version string for Model.
+FOUNDATION_EXPORT const unsigned char ModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Model/PublicHeader.h>
+
+
---
+++ Model/Provider/UserDefaultsProvider.swift
@@ -0,0 +1,67 @@
+//
+//  UserDefaultsProvider.swift
+//  Model
+//
+//  Created by Shunya Yamada on 2021/03/11.
+//
+
+import Foundation
+
+public final class UserDefaultsProvider {
+    
+    // MARK: Singletone
+        
+    public static let shared: UserDefaultsProvider = .init()
+    
+    // MARK: Properties
+    
+    private let userDefaults: UserDefaults
+    
+    public enum Key: String {
+        /// 以前のアプリで使用していた保存したデータ一覧を取得するためのキー
+        case oldList = "yourList"
+    }
+    
+    // MARK: Initializer
+    
+    private init() {
+        userDefaults = UserDefaults.standard
+    }
+    
+    // MARK: Save
+    
+    public func set(value: Any, forKey key: Key) {
+        userDefaults.set(value, forKey: key.rawValue)
+    }
+    
+    public func setEnum<T: RawRepresentable>(value: T, forKey key: Key) {
+        userDefaults.set(value.rawValue, forKey: key.rawValue)
+    }
+    
+    public func setEncodable<T: Encodable>(value: T, forKey key: Key) {
+        guard let data = try? JSONEncoder().encode(value) else { return }
+        userDefaults.set(data, forKey: key.rawValue)
+    }
+    
+    // MARK: Load
+    
+    public func object<T>(type: T.Type, forKey key: Key) -> T? {
+        return userDefaults.object(forKey: key.rawValue) as? T
+    }
+    
+    public func enumObject<T: RawRepresentable>(type: T.Type, forKey key: Key) -> T? {
+        guard let rawValue = userDefaults.object(forKey: key.rawValue) as? T.RawValue else { return nil }
+        return T.init(rawValue: rawValue)
+    }
+    
+    public func decodableObject<T: Decodable>(type: T.Type, forKey key: Key) -> T? {
+        guard let data = userDefaults.data(forKey: key.rawValue) else { return nil }
+        return try? JSONDecoder().decode(type, from: data)
+    }
+    
+    // MARK: Remove
+    
+    public func removeObject(forKey key: Key) {
+        userDefaults.removeObject(forKey: key.rawValue)
+    }
+}
