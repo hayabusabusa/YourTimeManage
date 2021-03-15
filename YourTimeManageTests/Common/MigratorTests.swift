470714c194bfb5882cd93083641911b9683952f0
hayabusabusa <hayabusabusaSY@gmail.com>
2021-03-15 23:45:20 +0900

    test: add main app test target

---
+++ YourTimeManageTests/Common/MigratorTests.swift
@@ -0,0 +1,32 @@
+//
+//  MigratorTests.swift
+//  YourTimeManageTests
+//
+//  Created by Shunya Yamada on 2021/03/15.
+//
+
+import XCTest
+
+class MigratorTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
