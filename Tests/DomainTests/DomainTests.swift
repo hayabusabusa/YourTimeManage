85970683d4d1d1098f90ae446fcc2202564cef43
hayabusabusa <hayabusabusaSY@gmail.com>
2021-07-20 21:30:04 +0900

    chore: run swift test command

---
+++ Tests/DomainTests/DomainTests.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shunya Yamada on 2021/07/20.
+//
+
+import XCTest
+
+final class DomainTests: XCTestCase {
+    
+    func testCase() {
+        
+    }
+}
