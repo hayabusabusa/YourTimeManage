96d9bd66e6fa56752d2d2477837f81ae3deb0283
hayabusabusa <hayabusabusaSY@gmail.com>
2022-04-05 23:57:10 +0900

    feat: バージョン 2.0 用のマイグレーション処理を追加
    バージョン 1.x で保存されていたデータを読み込むための処理を追加。

---
+++ Package/Sources/Domain/Migration/V200Migrator.swift
@@ -0,0 +1,58 @@
+//
+//  V200Migrator.swift
+//  
+//
+//  Created by Shunya Yamada on 2022/04/05.
+//
+
+import Core
+import Foundation
+
+/// バージョン `1.x` 系のアプリからアップデートする際に実行するマイグレーション.
+public struct V200Migrator: MigratorProtocol {
+    public typealias Output = [YourStudyData]
+
+    /// 旧アプリのデータを `UserDefauls` に保存しているかどうか.
+    public var isStoredOldData: () -> Bool
+    /// 旧データを変換するマイグレーションを実行する.
+    public var migration: () -> Result<[YourStudyData], Error>
+
+    public func isNeedMigration() -> Bool {
+        return isStoredOldData()
+    }
+
+    public func migration(completion: @escaping (Result<[YourStudyData], Error>) -> Void) {
+        completion(migration())
+    }
+}
+
+public enum V200MigrationError: Error {
+    case failed(message: String)
+}
+
+public extension V200Migrator {
+
+    static func live(userDefaultsService: UserDefaultsService) -> Self {
+        return Self.init(
+            isStoredOldData: {
+                return userDefaultsService.object(.oldList) != nil
+            },
+            migration: {
+                // ここが `nil` の場合、以前のデータは存在しないのでマイグレーションの必要なし.
+                if let data = userDefaultsService.object(.oldList) as? Data {
+                    // ここが `nil` の場合、データの取得に失敗している可能性がある.
+                    do {
+                        // モジュール名が変わるため `setClass(_:forClassName:)` でクラス名を紐付けしておく.
+                        NSKeyedUnarchiver.setClass(YourStudyData.self, forClassName: YourStudyData.className)
+                        if let unarchivedData = try NSKeyedUnarchiver.unarchiveTopLevelObjectWithData(data) as? [YourStudyData] {
+                            return .success(unarchivedData)
+                        }
+                    } catch {
+                        return .failure(V200MigrationError.failed(message: "\(error)"))
+                    }
+                }
+                return .success([])
+            }
+        )
+    }
+}
